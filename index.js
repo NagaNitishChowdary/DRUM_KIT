50592532a756062073919d51601f7a35ebb632a5
Naga Nitish <95007559+NagaNitishChowdary@users.noreply.github.com>
2023-06-20 23:51:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,47 +5,55 @@ for(var i=0 ; i<drums ; i++){
         
         var clicked = this.innerHTML ; 
 
-        switch(clicked){
-
-            case "w" : 
-                var tom1 = new Audio("sounds/tom-1.mp3") ;
-                tom1.play() ; 
-            break ; 
-
-            case "a" : 
-                var tom1 = new Audio("sounds/tom-2.mp3") ;
-                tom1.play() ; 
-            break ; 
-
-            case "s" : 
-                var tom1 = new Audio("sounds/tom-3.mp3") ;
-                tom1.play() ; 
-            break ; 
-
-            case "d" : 
-                var tom1 = new Audio("sounds/tom-4.mp3") ;
-                tom1.play() ; 
-            break ; 
-
-            case "j" : 
-                var tom1 = new Audio("sounds/snare.mp3") ;
-                tom1.play() ; 
-            break ; 
-
-            case "k" : 
-                var tom1 = new Audio("sounds/crash.mp3") ;
-                tom1.play() ; 
-            break ; 
-
-            case "l" : 
-                var tom1 = new Audio("sounds/kick-bass.mp3") ;
-                tom1.play() ; 
-            break ; 
-
-            default :
-                console.log("not mentioned the sound") ;
-
-        }
+        makeSound(clicked) ; 
 
     }) ; 
+}
+
+document.addEventListener("keypress" , function(event){
+    makeSound(event.key) ; 
+});
+
+function makeSound(key){
+    switch(key){
+
+        case "w" : 
+            var tom1 = new Audio("sounds/tom-1.mp3") ;
+            tom1.play() ; 
+        break ; 
+
+        case "a" : 
+            var tom1 = new Audio("sounds/tom-2.mp3") ;
+            tom1.play() ; 
+        break ; 
+
+        case "s" : 
+            var tom1 = new Audio("sounds/tom-3.mp3") ;
+            tom1.play() ; 
+        break ; 
+
+        case "d" : 
+            var tom1 = new Audio("sounds/tom-4.mp3") ;
+            tom1.play() ; 
+        break ; 
+
+        case "j" : 
+            var tom1 = new Audio("sounds/snare.mp3") ;
+            tom1.play() ; 
+        break ; 
+
+        case "k" : 
+            var tom1 = new Audio("sounds/crash.mp3") ;
+            tom1.play() ; 
+        break ; 
+
+        case "l" : 
+            var tom1 = new Audio("sounds/kick-bass.mp3") ;
+            tom1.play() ; 
+        break ; 
+
+        default :
+            console.log("not mentioned the sound") ;
+
+    }
 }
